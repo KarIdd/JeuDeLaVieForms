2993fe756c8ba586a4c772b19044a2bb5ca7dd2d
FLATRES Johan <76738358+KarIdd@users.noreply.github.com>
2021-10-20 16:02:27 +0200

    Initial commit

---
+++ JeuDeLaVieForms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Jeu_de_la_vie
+{
+    class Program
+    {
+        /*
+        static void Main()
+        {
+
+            Game game = new Game(10, 10);
+
+            game.RunGameConsole();
+        }*/
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
